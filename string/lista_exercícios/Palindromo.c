e09397814f4a66fcc36c361d6d6f5ad8f9308f42
KelvsonUnicap <162983215+KelvsonUnicap@users.noreply.github.com>
2024-04-29 01:28:07 +0000

    teste 2.0

--- string/lista_exercícios/Palindromo.c
+++ string/lista_exercícios/Palindromo.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define MAX 60
 
-int check_palindromo(char frase[]){
-    char palindromo[MAX];
-    char copia[MAX];
+void display(char frase[]){
+    for(int i = 0 ; i < strlen(frase); i ++){
+        printf("%c", frase[i]);
+    }
+}
 
-    strcpy(frase,copia);
-
-    for(int i = strlen(frase), j = 0; i>=0; i--){
-        if(frase[i] == ' '){ continue; }
-        else{
-            palindromo[j++] = frase[i]; 
+int len(char text[]){
+    int tamanho_sem_espaco = 0;
+    for(int i = 0; i<strlen(text); i++){
+        if(text[i] == ' '){
+            continue;
         }
+        tamanho_sem_espaco++;
+    }
+    
+    return tamanho_sem_espaco;
+}
+
+int check_palindromo(char frase[]){
+    int tamanho_sem_espacos = len(frase);
+    char primitiva_sem_espaco[tamanho_sem_espacos], copia[tamanho_sem_espacos];
+
+    for(int i = 0 , j= 0; i < strlen(frase); i++){
+        if(frase[i] == ' '){ continue; }
+        primitiva_sem_espaco[j] = frase[i];
+        j++;
     }
 
-    for(int i = 0 ; i<=strlen(frase); i ++){
-        printf("%c", copia[i]);
+    for(int i = 0, j=0; i<=tamanho_sem_espacos; i++){
+        copia[j] = primitiva_sem_espaco[tamanho_sem_espacos - i];
+        j++;
     }
+
+    display(primitiva_sem_espaco);
 }
 
 void main(){
